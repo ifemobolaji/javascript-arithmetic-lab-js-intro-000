1184699747ae1c371c98cdba1fa2fa42f8383e60
loyal-transpiler-0513 <ifeawoyale@yahoo.com>
2020-02-10 00:55:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,6 +33,11 @@ function decrement(n) {
 function hey() {
   return g
 }
-function makeInt(n) {
-  return parseInt(n, 10)
+function makeInt('n') {
+  return parseInt('n', 10)
+}
+
+function preserveDecimal('n') {
+  return parseFloat('n')
+  
 }
